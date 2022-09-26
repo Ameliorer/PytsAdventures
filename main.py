4cc4d81998ae5d5502d681878388551b03780e88
Poutchy <enzo.mauti@laposte.net>
2022-09-26 18:40:32 +0200

    ajout des codes permettant l'affichages et les déplacements du joueur

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from sys import exit
 from debug import debug
+from player import Player
 
 pygame.init()
+sprite1 = pygame.sprite.Group()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Pyt's Adventures")
 clock = pygame.time.Clock()
 
-screen.fill('white')
+player = Player(sprite1)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+    screen.fill('white')
+    sprite1.update()
+    sprite1.draw(screen)
     pygame.display.update()
     clock.tick(60)
