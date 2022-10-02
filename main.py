9af82285c290196430b9b462a663ef314e35d701
Amélie HACQUE <95433250+Ameliorer@users.noreply.github.com>
2022-10-02 22:46:00 +0200

    ajout de la class des objets statiques

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sys import exit
 import time
 from debug import debug
 from player import Player
-from wall import Wall
 
 pygame.init()
 pygame.display.set_caption("Pyt's Adventures")
@@ -12,12 +11,13 @@ screen_width, screen_height = 1280, 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 sprite1 = pygame.sprite.Group()
+collisions = pygame.sprite.Group()
+walls = pygame.sprite.Group()
 
 
-walls = [
-    pygame.Rect(0, 0, 1280, 20), pygame.Rect(0, 0, 20, 720),
-    pygame.Rect(0, 700, 1280, 20), pygame.Rect(1260, 0, 20, 720),
-    ]
+StaticObstacle((0,0),(1280,20),[walls,collisions])
+StaticObstacle((0, 700),(1280,20),[walls,collisions])
+
 
 
 
@@ -34,8 +34,8 @@ while True:
     sprite1.update()
     sprite1.draw(screen)
 
-    for wall in walls:
-        pygame.draw.rect(screen, (0, 0, 0), wall)
+    #    for wall in walls:
+    #    pygame.draw.rect(screen, (0, 0, 0), wall)
 
     pygame.display.update()
     clock.tick(60)
