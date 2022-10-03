e80442fe07ea004fb13f933c5fe19c386f3ee6c0
Poutchy <enzo.mauti@laposte.net>
2022-10-03 18:46:08 +0200

    création d'un test pour le fantome, et ajout de la fonction kill lors de la fin de l'execution

--- fantome.py
+++ fantome.py
@@ -1,18 +1,21 @@
 import pygame
 
-class fantome(pygame.sprite.Sprite):
+class Fantome(pygame.sprite.Sprite):
     def __init__(self, groups, listePos) -> None:
-        super().__init__(*groups)
+        super().__init__(groups)
         self.compteur = 0
         self.passage = listePos
         self.image = pygame.Surface((30, 60))
         self.image.fill('red')
 
-        self.rect = self.image.get_rect(topleft=self.passage[self.compteur])
+        self.rect = self.image.get_rect(topleft=(self.passage[self.compteur][0], self.passage[self.compteur][1]))
     
-    def run(self):
+    def update(self, dt):
         self.compteur += 1
 
-        if self.compteur < self.passage.length:
-            self.rect.x = self.passage[self.compteur].x
-            self.rect.y = self.passage[self.compteur].y
+        if self.compteur < len(self.passage):
+            self.rect.x = self.passage[self.compteur][0]
+            self.rect.y = self.passage[self.compteur][1]
+        
+        else:
+            self.kill()
