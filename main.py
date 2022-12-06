cc23857b170eda61711219cde64ff27870138a1b
Amélie HACQUE <95433250+Ameliorer@users.noreply.github.com>
2022-12-06 18:12:53 +0100

    bug fixation

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
             #game.destroy()
             #game.__init__(1, screen)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.Rect.collidepoint(game.button,pygame.mouse.get_pos()):
+            if (game.game_over and pygame.Rect.collidepoint(game.button,pygame.mouse.get_pos())):
                 game.destroy()
                 game.__init__(1, screen)
     game.update()
